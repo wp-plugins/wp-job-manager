026e91fb2faef0a8d643e3f8599081d67350909d
mikejolley <mikejolley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-08 12:46:18 +0000

    Tagging 1.7.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-job-manager/trunk@853699 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/job-submission.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {
-	jQuery( '.job-manager-remove-uploaded-image' ).click(function() {
-		jQuery( '.job-manager-uploaded-image' ).remove();
-		return false;
-	});
-});
--- uninstall.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-wp_clear_scheduled_hook( 'job_manager_check_for_expired_jobs' );
