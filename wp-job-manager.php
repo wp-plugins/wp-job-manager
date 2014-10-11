810c1db53c9ebbf0129ef65cecbb328b5075c4ac
mikejolley <mikejolley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-11 21:45:33 +0000

    Release 1.16.1, see readme.txt for changelog.
    git-svn-id: https://plugins.svn.wordpress.org/wp-job-manager/tags/1.16.1@1005834 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-job-manager.php
+++ wp-job-manager.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Job Manager
 Plugin URI: https://wpjobmanager.com/
 Description: Manage job listings from the WordPress admin panel, and allow users to post jobs directly to your site.
-Version: 1.16.0
+Version: 1.16.1
 Author: Mike Jolley
 Author URI: http://mikejolley.com
 Requires at least: 3.8
@@ -31,7 +31,7 @@ class WP_Job_Manager {
 	 */
 	public function __construct() {
 		// Define constants
-		define( 'JOB_MANAGER_VERSION', '1.16.0' );
+		define( 'JOB_MANAGER_VERSION', '1.16.1' );
 		define( 'JOB_MANAGER_PLUGIN_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 		define( 'JOB_MANAGER_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
 
@@ -93,6 +93,13 @@ class WP_Job_Manager {
 	 * Register and enqueue scripts and css
 	 */
 	public function frontend_scripts() {
+		$ajax_url = admin_url( 'admin-ajax.php', 'relative' );
+
+		// WPML workaround until this is standardized
+		if ( class_exists( 'SitePress' ) ) {
+			add_query_arg( 'lang', $GLOBALS['sitepress']->get_current_language(), $ajax_url );
+		}
+
 		wp_register_script( 'wp-job-manager-ajax-filters', JOB_MANAGER_PLUGIN_URL . '/assets/js/ajax-filters.min.js', array( 'jquery', 'chosen' ), JOB_MANAGER_VERSION, true );
 		wp_register_script( 'wp-job-manager-job-dashboard', JOB_MANAGER_PLUGIN_URL . '/assets/js/job-dashboard.min.js', array( 'jquery' ), JOB_MANAGER_VERSION, true );
 		wp_register_script( 'wp-job-manager-job-application', JOB_MANAGER_PLUGIN_URL . '/assets/js/job-application.min.js', array( 'jquery' ), JOB_MANAGER_VERSION, true );
@@ -101,7 +108,7 @@ class WP_Job_Manager {
 		wp_register_script( 'wp-job-manager-term-multiselect', JOB_MANAGER_PLUGIN_URL . '/assets/js/term-multiselect.min.js', array( 'jquery', 'chosen' ), JOB_MANAGER_VERSION, true );
 
 		wp_localize_script( 'wp-job-manager-ajax-filters', 'job_manager_ajax_filters', array(
-			'ajax_url' => admin_url( 'admin-ajax.php', 'relative' )
+			'ajax_url' => $ajax_url
 		) );
 		wp_localize_script( 'wp-job-manager-job-dashboard', 'job_manager_job_dashboard', array(
 			'i18n_confirm_delete' => __( 'Are you sure you want to delete this listing?', 'wp-job-manager' )
